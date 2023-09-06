53edd64276534805d7659edf10d1bd85b5efcff5
Ramanan <ratram2010@gmail.com>
2023-09-06 20:44:48 +0100

    roboshop terraform vpc

---
+++ main.tf
@@ -0,0 +1,27 @@
+module "vpc" {
+  source = "git::https://github.com/ramanan2017/tf-module-vpc.git"
+
+  for_each                   = var.vpc
+  cidr                       = each.value["cidr"]
+  subnets                    = each.value["subnets"]
+  default_vpc_id             = var.default_vpc_id
+  default_vpc_cidr           = var.default_vpc_cidr
+  default_vpc_route_table_id = var.default_vpc_route_table_id
+  tags                       = var.tags
+  env                        = var.env
+}
+
+module "alb" {
+  source = "git::https://github.com/ramanan2017/tf-module-alb.git"
+  for_each                   = var.alb
+  internal                   = each.value["internal"]
+  lb_type                    = each.value["lb_type"]
+  sg_ingress_cidr            = each.value["sg_ingress_cidr"]
+  vpc_id                     = each.value["internal"] ? local.vpc_id: var.default_vpc_id
+  subnets                    =  each.value["internal"] ? data.aws_subnets.subnets.ids : local.app_subnets
+  tags                       = var.tags
+  env                        = var.env
+  sg_port                    = each.value["sg_port"]
+
+}
+
